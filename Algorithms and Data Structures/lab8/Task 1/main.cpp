da916643a3ec44169f9c43b740e43e6a9ba1e82d
nazzrrg <nazarov.egor2010@yandex.ru>
2020-02-15 14:24:11 +0300

    Lab 1 Task 1 OK

---
+++ Algorithms and Data Structures/lab8/Task 1/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+    std::freopen("input.txt", "r", stdin);
+    std::freopen("output.txt", "w", stdout);
+
+    uint16_t n,m;
+    std::cin >> n >> m;
+    std::vector<std::vector<uint8_t>> matrix(n);
+    for (auto& v : matrix) {
+        v.resize(n);
+    }
+
+    for (uint16_t t1, t2, i = 0; i < m; i++) {
+        std::cin >> t1 >> t2;
+        matrix[--t1][--t2] = 1;
+    }
+
+    for (const auto& v : matrix) {
+        for (auto a : v) {
+            std::cout << int(a) << ' ';
+        }
+        std::cout << std::endl;
+    }
+    return 0;
+}
