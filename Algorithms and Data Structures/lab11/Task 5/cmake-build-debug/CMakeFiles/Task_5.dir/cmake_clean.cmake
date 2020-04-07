90de53434bb76e3507e73f3ec8dd329d99fed29d
nazzrrg <nazarov.egor2010@yandex.ru>
2020-04-07 08:37:04 +0500

    Lab 11 Task 3 OK

---
+++ Algorithms and Data Structures/lab11/Task 5/cmake-build-debug/CMakeFiles/Task_5.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Task_5.dir/main.cpp.o"
+  "Task_5"
+  "Task_5.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Task_5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Algorithms and Data Structures/lab11/Task 5/main.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+const int64_t INF = 8e18;
+
+struct edge {
+    int from, to;
+    int64_t cost;
+    edge(int from, int to, int64_t cost) : from(from), to(to), cost(cost) {};
+    edge() = default;
+};
+
+void dfs(int i, vector<vector<int>>& g, vector<bool>& used) {
+    used[i] = true;
+    for (auto to : g[i]) {
+        if (!used[to])
+            dfs(to, g, used);
+    }
+}
+
+void ford(vector<edge>& e, int v, int n, int m, vector<vector<int>>& g) {
+    vector<int64_t> d (n, INF);
+    d[v] = 0;
+    vector<int> p (n, -1);
+    int x = -1;
+    for (int i = 0; i < n; ++i) {
+        x = -1;
+        for (auto &ee : e) {
+            if (d[ee.from] < INF)
+                if (d[ee.to] > d[ee.from] + ee.cost) {
+                    d[ee.to] = max(-INF, d[ee.from] + ee.cost); // от переполнения вниз
+                    p[ee.to] = ee.from;
+                    x = ee.to;
+                }
+        }
+    }
+
+    vector<bool> used(n, false);
+
+    if (x != -1) {
+        for (int i = 0; i < n; ++i)
+            x = p[x]; // так мы точно упремся в цикл в силу того что просматриваем все ребра
+        dfs(x, g, used); // обойдем цикл
+    }
+    for (int i = 0; i < n; ++i) {
+        if(used[i]) {
+            cout << '-' << endl;
+        } else if (d[i] == INF) {
+            cout << '*' << endl;
+        } else {
+            cout << d[i] << endl;
+        }
+    }
+}
+
+int main() {
+    freopen("negcycle.in", "r", stdin);
+    freopen("negcycle.out", "w", stdout);
+    int n, m, s;
+    cin >> n >> m >> s;
+    vector<edge> e;
+    vector<vector<int>> g(n);
+    for (int i = 0; i < m; ++i){
+        int from, to;
+        int64_t cost;
+        cin >> from >> to >> cost;
+        e.emplace_back(--from, --to, cost);
+        g[from].emplace_back(to);
+    }
+
+    ford(e, --s, n, m, g);
+
+    return 0;
+}
